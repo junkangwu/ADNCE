e6ab76a975db10b731ed6f711f59a6de8af669b9
junkangwu <1041860981@qq.com>
2023-05-30 10:03:56 +0800

    update gitignore

--- graph/bash/tmp1.sh
+++
@@ -1,8 +0,0 @@
-gpus=$1
-w2=$2
-bash main_frame_robust.sh $gpus BZR random2 reweight 0.20 0.0 $w2 11 0.1 w1 0.1
-bash main_frame_robust.sh $gpus BZR random2 reweight 0.50 0.0 $w2 11 0.1 w1 0.2
-bash main_frame_robust.sh $gpus BZR random2 reweight 0.10 0.0 $w2 11 0.1 w1 0.3
-bash main_frame_robust.sh $gpus BZR random2 reweight 0.10 0.0 $w2 11 0.1 w1 0.4
-bash main_frame_robust.sh $gpus BZR random2 reweight 0.20 0.0 $w2 11 0.1 w1 0.5
-bash main_frame_robust.sh $gpus BZR random2 reweight 0.40 0.0 $w2 11 0.1 w1 0.6
